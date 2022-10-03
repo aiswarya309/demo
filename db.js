9a324ca34ff324a620ec3f9f8e6c467c1584c895
aiswarya309 <aishuzz309@gmail.com>
2022-10-03 18:15:31 +0530

    second

--- db.js
+++ db.js
@@ -1,10 +1,23 @@
 const mysql = require('mysql');
+const util = require('util');
 
-const db=mysql.createConnection({
+const config={
     user: "root",
     host:"localhost",
     password:"password",
     database:"company_webpage"
-})
+}
 
-module.exports = db;
+// module.exports = db;
+function makeDb(config) {
+  const conncetion = mysql.createConnection(config);
+  return {
+    query(sql, args) {
+      return util.promisify(connection.query).call(conncetion, sql, args);
+    },
+    close() {
+      return util.promisify(connection.end).call(connection);
+    },
+  };
+}
+module.exports = makeDb;
--- model/auth.js
+++ model/auth.js
@@ -1,15 +1,17 @@
-const db = require("../db")
-console.log("model.")
-async function signupModel(email,password,name){
-    console.log("signup model")
-    try{
-        let signupM=await db.query("INSERT INTO signup(email,password,name)values(?,?,?)",[email,password,name])
-        return true
-
-    }catch(err){
-        console.log("Error:-",err)
-    }finally {
-        await db.close();
-      }
+const db = require('../db');
+console.log('model.');
+async function signupModel(email, password, name) {
+  console.log('signup model');
+  try {
+    let signupM = await db.query(
+      'INSERT INTO signup(email,password,name)values(?,?,?)',
+      [email, password, name]
+    );
+    return res.status(200).json({ success: true, message: 'success' });
+  } catch (err) {
+    return res.status(500).json({ success: false, message: 'fail' });
+  } finally {
+    await makeDb.close();
+  }
 }
-module.exports=signupModel
+module.exports = signupModel;
