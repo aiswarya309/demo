e8126ae69d800c2f4287a1cd8b37bc4905677075
aiswarya309 <aishuzz309@gmail.com>
2022-10-04 10:46:23 +0530

    push

--- controller/auth.js
+++ controller/auth.js
@@ -6,16 +6,19 @@ async function signupController(req, res) {
     let email = req.body.email;
     let password = req.body.password;
     let name = req.body.name;
-    if (email === '' && password === '' && name === '') {
+    if (email == '' && password == '' && name == '') {
       return res.status(500).json({ success: false, message: 'fail' });
-    }
+    }else{
     console.log('controller data', email, password, name);
     const signupC = await signupModel(email, password, name);
-    if (signupC) {
-      return res.status(200).json({ success: true, message: 'success' });
-    } else {
-      return res.status(500).json({ success: false, message: 'fail' });
+    return res.status(200).json({ success: true, message: 'success' });
+
     }
+    // if (signupC) {
+    //   return res.status(200).json({ success: true, message: 'success' });
+    // } else {
+    //   return res.status(500).json({ success: false, message: 'fail' });
+    // }
   } catch (err) {
     console.log('Error:-', err);
   }
