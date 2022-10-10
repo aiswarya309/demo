0b491b4cc38ce97a157b918eebd7052061cc28a8
aiswarya309 <aishuzz309@gmail.com>
2022-10-10 12:21:06 +0530

    config,env,redux for signup page

--- controller/auth.js
+++ controller/auth.js
@@ -6,21 +6,45 @@ async function signupController(req, res) {
     let email = req.body.email;
     let password = req.body.password;
     let name = req.body.name;
-    if (email == '' && password == '' && name == '') {
-      return res.status(500).json({ success: false, message: 'fail' });
-    }else{
     console.log('controller data', email, password, name);
+    // console.log(req)
     const signupC = await signupModel(email, password, name);
-    return res.status(200).json({ success: true, message: 'success' });
-
+    if (signupC) {
+      return res.status(200).json({ success: true, message: 'success' });
+    } else {
+      return res.status(200).json({ success: false, message: 'fail' });
     }
-    // if (signupC) {
-    //   return res.status(200).json({ success: true, message: 'success' });
-    // } else {
-    //   return res.status(500).json({ success: false, message: 'fail' });
-    // }
   } catch (err) {
     console.log('Error:-', err);
   }
 }
 module.exports = { signupController };
+
+// // const { user } = require('../config');
+// const signupModel = require('../model/auth');
+// console.log('controller.js');
+// async function signupController(req, res) {
+//   console.log('signup data');
+//   try {
+//      let email = req.body.params.email;
+//      let password = req.body.params.password;
+//      let name = req.body.params.name;
+//      console.log("signup data:-",email,password,name)
+//     if (email == '' && password == '' && name == '') {
+//       return res.status(500).json({ success: false, message: 'fail' });
+//     }else{
+//     console.log('controller data', email, password, name);
+//     const signupC = await signupModel(email, password,name);
+//     return res.status(200).json({ success: true, message: 'success' });
+
+//     }
+//     // if (signupC) {
+//     //   return res.status(200).json({ success: true, message: 'success' });
+//     // } else {
+//     //   return res.status(500).json({ success: false, message: 'fail' });
+//     // }
+//   } catch (err) {
+//     console.log('Error:-', err);
+//   }
+// }
+// module.exports = { signupController };
--- model/auth.js
+++ model/auth.js
@@ -1,17 +1,49 @@
-const db = require('../db');
-console.log('model.');
-async function signupModel(email, password, name) {
-  console.log('signup model');
-  try {
-    let signupM = await db.query(
-      'INSERT INTO signup(email,password,name)values(?,?,?)',
-      [email, password, name]
-    );
-    return res.status(200).json({ success: true, message: 'success' });
-  } catch (err) {
-    return res.status(500).json({ success: false, message: 'fail' });
-  } finally {
-    await makeDb.close();
-  }
+const {makeDb} = require("../db")
+console.log("model.")
+async function signupModel(email,password,name){
+  const db=makeDb()
+    console.log("signup model")
+    try{
+        let signupM=await db.query("INSERT INTO signup(email,password,name)values(?,?,?)",[email,password,name])
+        return true
+
+    }catch(err){
+        console.log("Error:-",err)
+    }finally {
+        await db.close();
+      }
 }
-module.exports = signupModel;
+module.exports=signupModel
+
+
+// const makeDb = require('../db');
+// const {host,user,password,database,port}=require('../config')
+// console.log('model.');
+// console.log("host",config.host)
+// console.log("user", config.user)
+// console.log("password:",config.password)
+// console.log("database:",config.database)
+// console.log("port:",port)
+
+// async function signupModel(email, password, name) {
+//   console.log('.......signup model......',email,password,name);
+//   const db= await  makeDb()
+//   try {
+//     // await db.query()
+//     let signupM = await db.query(
+//       'INSERT INTO signup(email,password,name)values(?,?,?)',
+//       [email, password, name]
+//     );
+//     // await db.close()
+//     console.log('--------model.----------',signupM);
+//     return true
+//   } catch (err) {
+//     return false
+//   }
+//   // finally {
+//   //   await db.close();
+//   // }
+// }
+// signupModel('1qw', 'dgdg', 'hjhjh')
+
+// module.exports = signupModel;
